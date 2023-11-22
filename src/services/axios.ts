739ea8aae8f0f40b8f7b151e7495424797b0fc15
jarji <100996174+jarjia@users.noreply.github.com>
2023-11-22 14:07:48 +0400

    commit

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: "https://api-movie-quotes.jarji-abuashvili.link",
+  baseURL: "https://api-your.ecommerce.jarji-abuashvili.link",
   withCredentials: true,
 });
 
